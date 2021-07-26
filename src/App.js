4788586c92aaf34618dbad07786246e8e99788c4
Femi1992 <olufemidokun@hotmail.co.uk>
2021-07-26 09:17:04 +0100

    added changes to boolean logic throughout code

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends React.Component {
         orderTotal: 0,
         dinerOne: [],
         dinerTwo: [],
-        dinerOneHasMain: false,
-        dinerTwoHasMain: false,
-        cheesecakeOrdered: false,
-        basketHidden: true,
+        dinerOneHasMain: Boolean(false),
+        dinerTwoHasMain: Boolean(false),
+        cheesecakeOrdered: Boolean(false),
+        basketHidden: Boolean(true),
         currDiner: 1
     };
 
@@ -40,29 +40,31 @@ class App extends React.Component {
         let currentBasket = this.state.basket
         let currentTotal = this.state.orderTotal
 
-        if(!this.checkWaiter(item) && !this.checkCourseSelection(item) && this.canOrderCheesecake){
+        if(item == "Cheesecake") {
+            this.canOrderCheesecake()
+        }
+        if(!this.checkWaiter(item) && !this.checkCourseSelection(item)){
             currentBasket.push(item)
             let newTotal = currentTotal += price
             this.setState({
                 basket: currentBasket, 
                 orderTotal: newTotal,
-                basketHidden: false
+                basketHidden: Boolean(false)
             })
+            this.canContinueOrder()
         }
-        this.canContinueOrder()
     }
 
-    //TODO need to ensure logic here is working properly
     continueDinerOrder = () => {
         let currentDiner = this.state.currDiner
         let continueOrder = confirm("Would you like to order more for Diner" + currentDiner)
         if(continueOrder && currentDiner == 1 && this.state.dinerOne.length == 3){
-            alert("Diner", currentDiner, "has ordered the maximum courses")
+            alert("Diner" + currentDiner + "has ordered the maximum courses")
             this.setState({
                 currDiner : 2
             })
         } else if(continueOrder && currentDiner == 2 && this.state.dinerTwo.length == 3){
-            alert("Diner", currentDiner, "has ordered the maximum courses")
+            alert("Diner" + currentDiner + "has ordered the maximum courses")
 
         } else if(!continueOrder  && currentDiner == 1 && this.state.dinerOne.length < 2) {
             alert("Each Diner must select at least two courses, please chooose another")
@@ -77,13 +79,15 @@ class App extends React.Component {
             })
 
         } else if(!continueOrder && currentDiner == 2  && this.state.dinerTwo.length == 2) {
-            this.checkCourseMain()
+            if(this.checkCourseMain()){
+                return true
+            }
         }
     }
 
     canContinueOrder = () => {
         if(this.state.currDiner == 1 && this.state.dinerOne.length < 3) {
-            this.continueDinerOrder()
+            this.continueDinerOrder() 
         } else if (this.state.dinerTwo.length < 3) {
             this.continueDinerOrder()
         } else {
@@ -91,20 +95,32 @@ class App extends React.Component {
         }
     }
 
-    //TODO stop diner from odering the from the same course, so you cant have two starters
     checkCourseSelection = (item) => {
         let dinerOneCourses = this.state.dinerOne;
         let dinerTwoCourses = this.state.dinerTwo;
+        let allCourses = [this.state.startersLs, this.state.mainsLs, this.state.dessertsLs]
 
-        if (this.state.currDiner == 1 && dinerOneCourses.indexOf(item) != -1) {
+        //check that they are not odering from the same course for each diner
+        for(let x=0; x < allCourses.length; x++){
+            if(allCourses[x].includes(item) && dinerOneCourses.some(item => allCourses[x].includes(item)) && this.state.currDiner == 1){
+                alert("You have already ordered from this section for this diner!")
+                return Boolean(true)
+            } else if (allCourses[x].includes(item) && dinerTwoCourses.some(item => allCourses[x].includes(item)) && this.state.currDiner == 2) {
+                alert("You have already ordered from this section for this diner!")
+                return Boolean(true)
+            }
+        }
+        
+        //check they have not already ordered this specific course
+        if (this.state.currDiner == 1 && dinerOneCourses.includes(item)) {
             alert("This course has already been selected for this diner, choose another please!")
-            return true
-        } else if (dinerTwoCourses.indexOf(item) != -1) {
+            return Boolean(true)
+        } else if (dinerTwoCourses.includes(item)) {
             alert("This course has already been selected for this diner, choose another please!")
-            return true
+            return Boolean(true)
         } else {
             this.addToDinerOrder(item);
-            return false
+            return Boolean(false)
         }
     }
 
@@ -113,27 +129,27 @@ class App extends React.Component {
         if(this.state.currDiner == 1){
             if(!this.state.dinerOne.some(item => mains.includes(item))){
                 alert("You have not selected a main meal for Diner One!")
-                return true
+                return Boolean(false)
             }
         } else if (this.state.currDiner == 2) {
             if(!this.state.dinerTwo.some(item => mains.includes(item))){
                 alert("You have not selected a main meal for Diner Two!")
-                return true
+                return Boolean(false)
             }
         } else {
-            return false
+            return Boolean(true)
         }
     }
 
-    canOrderCheesecake = (item) => {
-        if(item == "Cheesecake" && !this.state.cheesecakeOrdered){
+    canOrderCheesecake = () => {
+        if(!this.state.cheesecakeOrdered){
             this.setState({
-                cheesecakeOrdered: true
+                cheesecakeOrdered: Boolean(true)
             })
-            return true
+            return Boolean(true)
         } else {
             alert("Sorry, no cheesecake left")
-            return false
+            return Boolean(false)
         }
     }
 
@@ -144,9 +160,9 @@ class App extends React.Component {
         if(this.state.servingWaiter == "Pierre" && PierreLs.includes(item)){
             if(dinerOne.includes("Salmon fillet") || (dinerOne.includes("Prawn cocktail")) || (dinerTwo.includes("Prawn cocktail")) || (dinerTwo.includes("Prawn cocktail"))) {
                 alert("Pierre does not like salmon mixed with Prawn cocktail")
-                return true
+                return Boolean(true)
             } else {
-                return false
+                return Boolean(false)
             }
         }
     }
@@ -178,7 +194,7 @@ class App extends React.Component {
                     addBasket={this.addMenuItemBasket}
                 />    
 
-                {this.state.basketHidden ? null : <Basket isHidden={true}
+                {this.state.basketHidden ? null : <Basket isHidden={Boolean(true)}
                     total={this.state.orderTotal}
                     Basket={this.state.basket}
                 />}
